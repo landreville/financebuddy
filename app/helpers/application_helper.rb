fa46313d9c5df5ea3a73dcdd608a387baec77a05
Landreville <landreville@deadtreepages.com>
2026-05-03 13:17:01 -0400

    feat: rewrite sidebar with dark group headers and net worth

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def fmt_money(n, sign: false, blank_zero: false)
+    return "".html_safe if blank_zero && (!n || n.abs < 0.005)
+    abs = number_to_currency(n.abs, unit: "", precision: 2)
+    if sign
+      prefix = n < 0 ? "-" : n > 0 ? "+" : " "
+      "#{prefix}$#{abs}".html_safe
+    else
+      prefix = n < 0 ? "-" : ""
+      "#{prefix}$#{abs}".html_safe
+    end
+  end
 end
