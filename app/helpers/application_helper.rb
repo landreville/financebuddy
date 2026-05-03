fc4ef20b2785d9eef721f674a3afd2cffa90085e
Landreville <landreville@deadtreepages.com>
2026-05-03 18:38:04 -0400

    fix: resolve lint issues in fmt_money helper and font-size unit for Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,17 @@ module ApplicationHelper
   def fmt_money(n, sign: false, blank_zero: false)
     return "".html_safe if blank_zero && (!n || n.abs < 0.005)
     abs = number_to_currency(n.abs, unit: "", precision: 2)
-    if sign
-      prefix = n < 0 ? "-" : n > 0 ? "+" : " "
-      "#{prefix}$#{abs}".html_safe
+    prefix = if sign
+      if n < 0
+        "-"
+      elsif n > 0
+        "+"
+      else
+        " "
+      end
     else
-      prefix = n < 0 ? "-" : ""
-      "#{prefix}$#{abs}".html_safe
+      (n < 0) ? "-" : ""
     end
+    "#{prefix}$#{abs}".html_safe
   end
 end
